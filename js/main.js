ca94453b6a0e4008e1314c4d05c2361b52fa0aad
Rohith Mohan <mohanrohith@gmail.com>
2012-12-12 21:15:38 +0530

    Added more sections and photo. Project section has to be filled

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
-/**/
+/*jslint debug: true, es5: true, unparam: false, sloppy: true, browser: true, windows: true, indent: 4, plusplus: true, nomen: true, newcap: true */
+/*global jQuery */
 
 
-(function($){
-    $(document).ready(_onReady);
+(function ($) {
+    "use strict";
+    
+    
 
     function _onReady() {
-        console.log("Yeah...");
-    }        
+        
+        //Bookmarks scrolling
+        $(".nav a").click(function (e) {
+            var $this = $(this),
+                href = $this.attr("href");
+            
+            if (href.length > 1) {
+                
+                e.preventDefault();
+                
+                $("html,body").animate({
+                    "scrollTop" : $(href).offset().top - 60
+                }, "fast");
+            }
+            
+        });
+        
+        
+        //Collapsing
+        $(".row h2").bind("click", function (e) {
+            var $this = $(this);
+            
+            $this.find("i").toggleClass("icon-chevron-down icon-chevron-right");
+            
+            $this.next(".block").toggleClass("closed").collapse("toggle");
+        });
+        
+    }
+    
+    $(document).ready(_onReady);
 }(jQuery));
